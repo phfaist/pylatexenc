4cc487aca001e9815fe1bd4522d372140eba2dba
Philippe Faist <pfaist@ethz.ch>
2015-05-06 00:15:10 +0200

    setuptools set up

--- setup.py
+++ setup.py
@@ -1,6 +1,39 @@
+#
+# The MIT License (MIT)
+# 
+# Copyright (c) 2015 Philippe Faist
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import os
+import os.path
+import sys
+
 from setuptools import setup, find_packages
 
 from pylatexenc.version import version_str
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
 
 setup(
     name = "pylatexenc",
@@ -10,31 +43,25 @@ setup(
     author = "Philippe Faist",
     author_email = "philippe.faist@bluewin.ch",
     description = "Python library for encoding unicode to latex and for parsing LaTeX to generate unicode text",
+    long_description = read("README.rst"),
     license = "MIT",
     keywords = "latex text unicode encode parse expression",
     url = "https://github.com/phfaist/pylatexenc",
     classifiers=[
-        'Development Status :: 5 - Stable',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
+        'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering',
         'Topic :: Text Processing :: General',
         'Topic :: Text Processing :: Markup :: LaTeX',
     ],
 
-    # could also include long_description, download_url, classifiers, etc.
-
+    # files
     packages = find_packages(),
     scripts = [],
-
     install_requires = [],
-
     package_data = {
     },
-
-    
 )
