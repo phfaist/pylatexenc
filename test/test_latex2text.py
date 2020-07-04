f3734b65cd8f7d001887718d242871ace1e20e0c
Philippe Faist <phfaist@gmail.com>
2020-07-04 23:35:02 +0200

    update tests

--- test/test_latex2text.py
+++ test/test_latex2text.py
@@ -588,7 +588,9 @@ above. """
 
     def test_repl_eqn(self):
 
-        for env in ('equation', 'eqnarray', 'align', 'multline', 'gather', 'dmath'):
+        for env in ('equation', 'equation*', 'eqnarray', 'eqnarray*',
+                    'align', 'align*', 'multline', 'multline*',
+                    'gather', 'gather*', 'dmath', 'dmath*'):
 
             self.assertEqualUpToWhitespace(
                 LatexNodes2Text(strict_latex_spaces='except-in-equations').latex_to_text(
